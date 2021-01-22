cb4e5b6c3de2cb235f3996a09c71dfda6be90fa1
EllaiSys <admin@ellaisys.com>
2021-01-22 15:03:28 +0530

    Fix: Error handlign of json response.

--- src/Auth/AuthenticatesUsers.php
+++ src/Auth/AuthenticatesUsers.php
@@ -63,7 +63,7 @@ trait AuthenticatesUsers
                 $response = $this->createLocalUser($credentials);
             } //End if
             
-            return $this->sendFailedLoginResponse($request, $e);
+            return $this->sendFailedLoginResponse($request, $e, $isJsonResponse);
         } catch (CognitoIdentityProviderException $e) {
             Log::error('AuthenticatesUsers:attemptLogin:CognitoIdentityProviderException');
             return $this->sendFailedCognitoResponse($e);
