0b48813d62ca8d2720453b0859d3f024abad2fdb
EllaiSys <admin@ellaisys.com>
2022-01-19 21:41:53 +0530

    fix: Error handling in the cognito middleware to manage web and api guards

--- src/Http/Middleware/AwsCognitoAuthenticate.php
+++ src/Http/Middleware/AwsCognitoAuthenticate.php
@@ -15,11 +15,14 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
+use Illuminate\Support\Facades\Log;
+
 use Exception;
 use Ellaisys\Cognito\Exceptions\AwsCognitoException;
 use Ellaisys\Cognito\Exceptions\NoTokenException;
 use Ellaisys\Cognito\Exceptions\InvalidTokenException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class AwsCognitoAuthenticate extends BaseMiddleware
 {
@@ -30,28 +33,38 @@ class AwsCognitoAuthenticate extends BaseMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, $module=null, $right=null)
+    public function handle(Request $request, Closure $next, $guards=null)
     {
+        $guard='';
+        $middleware='';
+        $countRouteMiddleware=0;
+
         try {
-
             $routeMiddleware = $request->route()->middleware();
-
-            if (empty($routeMiddleware) || (count($routeMiddleware)<1)) {
+            if (empty($routeMiddleware) || (($countRouteMiddleware=count($routeMiddleware))<1)) {
                 return response()->json(['error' => 'UNAUTHORIZED_REQUEST', 'exception' => null], 401);
+            } else {
+                ($countRouteMiddleware>0)?($guard = $routeMiddleware[0]):null;
+                ($countRouteMiddleware>1)?($middleware = $routeMiddleware[1]):null;
             } //End if
 
-            $this->authenticate($request);
+            //$this->authenticate($request);
             return $next($request);
         } catch (Exception $e) {
             if ($e instanceof NoTokenException) {
-                return response()->json(['error' => 'UNAUTHORIZED_REQUEST', 'exception' => 'NoTokenException'], 401);
+                return response()->json(['error' => 'UNAUTHORIZED_REQUEST', 'exception' => 'NoTokenException'], 401);             
             } //End if
 
             if ($e instanceof InvalidTokenException) {
                 return response()->json(['error' => 'UNAUTHORIZED_REQUEST', 'exception' => 'InvalidTokenException'], 401);
             } //End if
 
-            return response()->json(['error' => $e->getMessage()], 401);
+            //Raise error in case of generic error
+            if ($guard=='web') {
+                return redirect('/');
+            } else {
+                return response()->json(['error' => $e->getMessage()], 401);
+            } //End if
         } //Try-catch ends
     } //Function ends
 
