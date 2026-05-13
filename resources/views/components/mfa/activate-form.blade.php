58cfaa6cf947364e017140b0f450fc0e2f6c762f
EllaiSys <admin@ellaisys.com>
2026-05-13 16:32:35 +0530

    fix: update modal attributes for better accessibility and display

--- resources/views/components/mfa/activate-form.blade.php
+++ resources/views/components/mfa/activate-form.blade.php
@@ -1,8 +1,8 @@
 @if (Route::has('cognito.action.user.mfa.activate'))
     @if ($actionActivateMFA = session()->get('actionActivateMFA'))
     
-        <div class="modal fade show" id="modalMFAActivate" tabindex="-1" role="dialog"
-            aria-labelledby="modalMFAActivateLabel" style="display: block;">
+        <div class="modal fade show d-block" id="modalMFAActivate" tabindex="-1"
+            aria-labelledby="modalMFAActivateLabel" aria-hidden="true">
             <div class="card-body modal-dialog">
                 <form name="verify-mfa-code-form" id="verify-mfa-code-form"
                     method="post" action="{{route('cognito.action.user.mfa.activate')}}" autocomplete="off">
@@ -10,7 +10,7 @@
 
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">{{ __('MFA Code') }}- Activation QR Code</h5>
+                            <h5 id="modalMFAActivateLabel" class="modal-title">{{ __('MFA Code') }}- Activation QR Code</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         </div>
