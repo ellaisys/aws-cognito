1262d0fdb73d232494affa6a7abd490ba4f6244e
EllaiSys <admin@ellaisys.com>
2026-05-15 11:32:13 +0530

    docs: add license information to multiple files

--- src/Exceptions/AwsCognitoException.php
+++ src/Exceptions/AwsCognitoException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of AWS Cognito Auth solution.
+ *
+ * (c) EllaiSys <ellaisys@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ellaisys\Cognito\Exceptions;
 
 use Exception;
