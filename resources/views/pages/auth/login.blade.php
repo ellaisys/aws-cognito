a2e5391a48f33404522f0439626108ebd03856a6
EllaiSys <admin@ellaisys.com>
2026-06-13 16:31:29 +0530

    feat: add debug accordion for session and request data in login view

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -2,7 +2,34 @@
 
 @section('content')
 <div class="container">
-    @if (!((config('cognito.mfa')!='MFA_NONE') && (request()->has('status')) && (request()->has('session_token'))))
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+        @if(!empty(session()->all()))
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    Session Data (Debug)
+                </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">{{ print_r(session()->all(), true) }}</div>
+            </div>
+        </div>
+        @endif
+
+        @if(!empty(request()->all()))
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    Request Data (Debug)
+                </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">{{ print_r(request()->all(), true) }}</div>
+            </div>
+        </div>
+        @endif
+    </div>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -10,10 +37,12 @@
 
                 <div class="card-body">
                     <x-cognito::common.alert />
+
                     @php
                         $step = request()->route('step');
                         $step = (bool) config('cognito.allow_passkeys', false) ? $step : 'password';
                     @endphp
+
                     @switch($step)
                         @case('options')
                             <x-cognito::forms.auth.options-form />
@@ -33,7 +62,5 @@
             </div>
         </div>
     </div>
-    @endif
-    <x-cognito::mfa.code-form />
 </div>
 @endsection
