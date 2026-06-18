1d17756eb3b740076c68dd96c9c0814a35dae5d2
EllaiSys <admin@ellaisys.com>
2026-06-18 14:43:51 +0530

    refactor: remove debug session and request data display from login view

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -2,34 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div class="accordion accordion-flush" id="accordionFlushExample">
-        @if(!empty(session()->all()))
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                    Session Data (Debug)
-                </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">{{ print_r(session()->all(), true) }}</div>
-            </div>
-        </div>
-        @endif
-
-        @if(!empty(request()->all()))
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                    Request Data (Debug)
-                </button>
-            </h2>
-            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">{{ print_r(request()->all(), true) }}</div>
-            </div>
-        </div>
-        @endif
-    </div>
-
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
