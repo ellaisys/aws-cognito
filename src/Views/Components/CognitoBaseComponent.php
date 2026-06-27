788e277b8b5d2fed9b32e00a13943a3b6c046cba
EllaiSys <admin@ellaisys.com>
2026-06-27 23:26:30 +0530

    refactor: streamline username retrieval logic and add error handling for session and request data

--- src/Views/Components/CognitoBaseComponent.php
+++ src/Views/Components/CognitoBaseComponent.php
@@ -34,36 +34,100 @@ class CognitoBaseComponent extends Component
     {
         $username = 'cognito-user';
         
-        // Check authenticated claim data
-        $claim = session() ? session()->get('claim') : null;
-
-        // Check challenge data
-        $challengeData = session('data') ?? null;
+        // Check session data for username
+        $sessionUsername = $this->getSessionUsername();
 
         // Check request data for username
-        $requestUsername = request()->has('username') ? request()->get('username') : null;
+        $requestUsername = $this->getRequestUsername();
 
-        if ($claim && isset($claim['email'])) {
-            $username = $claim['email'];
-        } elseif ($challengeData && isset($challengeData['status'])
-            && $challengeData['status'] == 'challenge') {
-            
-            // If the challenge data contains a username
-            $challengeParamsValue = $challengeData['challenge_params'] ?? null;
-            if ($challengeParamsValue && isset($challengeParamsValue['USER_ID_FOR_SRP'])) {
-                $username = $challengeParamsValue['USER_ID_FOR_SRP'];
-            } elseif ($challengeParamsValue && isset($challengeParamsValue['USERNAME'])) {
-                $username = $challengeParamsValue['USERNAME'];
-            } else {
-                $username = $challengeData['username'] ?? $username;
-            }
+        if ($sessionUsername) {
+            $username = $sessionUsername;
         } elseif ($requestUsername) {
-                $username = $requestUsername;
+            $username = $requestUsername;
         } else {
-            $username = auth()->user() ? auth()->user()->email : $username;
+            $username = $this->getAuthenticatedUser() ?? $username;
         } // End if
 
         return $username;
+    } //Function end
+
+    /**
+     * Get the username from the session data
+     *
+     * @return string|null
+     */
+    private function getSessionUsername(): ?string
+    {
+        try {
+            // Check authenticated claim data
+            $claim = session() ? session()->get('claim') : null;
+
+            // Check challenge data
+            $challengeData = session('data') ?? null;
+
+            if ($claim && isset($claim['email'])) {
+                $username = $claim['email'];
+            } elseif ($challengeData && isset($challengeData['status'])
+                && $challengeData['status'] == 'challenge') {
+                
+                // If the challenge data contains a username
+                $challengeParamsValue = $challengeData['challenge_params'] ?? null;
+                if ($challengeParamsValue && isset($challengeParamsValue['USER_ID_FOR_SRP'])) {
+                    $username = $challengeParamsValue['USER_ID_FOR_SRP'];
+                } elseif ($challengeParamsValue && isset($challengeParamsValue['USERNAME'])) {
+                    $username = $challengeParamsValue['USERNAME'];
+                } else {
+                    $username = $challengeData['username'] ?? $username;
+                } // End if
+            } else {
+                $username = null;
+            } // End if
+            return $username;
+        } catch (Exception $exception) {
+            Log::error('CognitoBaseComponent:getSessionUsername:Exception');
+            throw $exception;
+        } //End try-catch
+    } //Function end
+
+    /**
+     * Get the username from the request data
+     *
+     * @return string|null
+     */
+    private function getRequestUsername(): ?string
+    {
+        try {
+            if (request()->has('username')) {
+                $username = request()->get('username');
+            } elseif (request()->has('email')) {
+                $username = request()->get('email');
+            } else {
+                $username = null;
+            }
+            return $username;
+        } catch (Exception $exception) {
+            Log::error('CognitoBaseComponent:getRequestUsername:Exception');
+            throw $exception;
+        } //End try-catch
+    } //Function end
+
+    /**
+     * Get the authenticated user
+     *
+     * @return string|null
+     */
+    private function getAuthenticatedUser(): ?string
+    {
+        try {
+            if (auth()->user()) {
+                return auth()->user()->email;
+            } else {
+                return null;
+            }
+        } catch (Exception $exception) {
+            Log::error('CognitoBaseComponent:getAuthenticatedUser:Exception');
+            throw $exception;
+        } //End try-catch
     } //Function end
 
     /**
