0c8dc654427a425bb027825c2b4647d191baa409
EllaiSys <admin@ellaisys.com>
2026-07-15 12:56:15 +0530

    refactor: remove unused validator method and simplify registration response

--- src/Http/Controllers/Auth/RegisterController.php
+++ src/Http/Controllers/Auth/RegisterController.php
@@ -65,20 +65,20 @@ class RegisterController extends Controller
         parent::__construct();
     } //Function ends
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    } //Function ends
+    // /**
+    //  * Get a validator for an incoming registration request.
+    //  *
+    //  * @param  array  $data
+    //  * @return \Illuminate\Contracts\Validation\Validator
+    //  */
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
+    //     ]);
+    // } //Function ends
 
     /**
      * Action to invite the a new user
@@ -106,8 +106,9 @@ class RegisterController extends Controller
                 } else {
                     $returnValue = redirect()
                         ->route($this->redirectPath())
-                        ->with('status', $this->statusMsg)
-                        ->with('message', trans($this->messageKey));
+                        ->with('status', 'success')
+                        ->with('message', trans($this->messageKey))
+                        ->with('data', $user);
                 } //End if
                 return $returnValue;
             } else {
