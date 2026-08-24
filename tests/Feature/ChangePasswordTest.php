ef7adff66b84bc479e80f03f24dcbdb5cfa7a5b6
EllaiSys <admin@ellaisys.com>
2026-08-24 22:11:27 +0530

    chore: updated changepassword test. wip

--- tests/Feature/ChangePasswordTest.php
+++ tests/Feature/ChangePasswordTest.php
@@ -1,17 +1,34 @@
 <?php
 
+/*
+ * This file is part of AWS Cognito Auth solution.
+ *
+ * (c) EllaiSys <ellaisys@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ellaisys\Cognito\Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Depends;
+use PHPUnit\Framework\Attributes\DependsExternal;
+
+use Ellaisys\Cognito\Enums;
 use Ellaisys\Cognito\Tests\TestCase;
+use Ellaisys\Cognito\Tests\Traits\AwsCognitoTrait;
 
 class ChangePasswordTest extends TestCase
 {
     /**
      * A basic test example.
      */
+    #[Test]
     public function test_the_application_returns_a_successful_response(): void
     {
         $this->assertTrue(true);
     }
-}
+} //Class ends
