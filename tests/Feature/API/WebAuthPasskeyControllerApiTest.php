51b2d34530a7b49a37e7b3ff463039d5c09660bd
EllaiSys <admin@ellaisys.com>
2026-09-15 20:03:56 +0530

    chore: update passkey tests for web and api

--- tests/Feature/API/WebAuthPasskeyControllerApiTest.php
+++ tests/Feature/API/WebAuthPasskeyControllerApiTest.php
@@ -11,17 +11,59 @@
 
 namespace Ellaisys\Cognito\Tests\Feature\API;
 
+use Illuminate\Support\Facades\Config;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\Group;
 
-#[Group('api')]
+use Ellaisys\Cognito\Enums;
+use Ellaisys\Cognito\Tests\Traits\AwsCognitoTrait;
+use Ellaisys\Cognito\Tests\Traits\AuthenticationTrait;
+
+#[Group('api'), Group('passkey'), Group('webauthn')]
 class WebAuthPasskeyControllerApiTest extends ApiTestCase
 {
+    use AwsCognitoTrait;
+    use AuthenticationTrait;
+
+    // Runs before each test method
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /**
+         * Override the configuration at runtime
+         */
+        Config::set('cognito.mfa_setup', 'OFF');
+        Config::set('cognito.mfa_type', ['SOFTWARE_TOKEN_MFA']);
+        Config::set('cognito.allowed_auth_flows', [
+            'ALLOW_REFRESH_TOKEN_AUTH',
+            'ALLOW_USER_PASSWORD_AUTH',
+            'ALLOW_USER_AUTH',
+        ]);
+
+        // Authenticate the user before running the tests
+        $this->authenticateApi();
+    } //Function ends
+
+    /**
+     * Test that the user pool client configuration allows for choice based
+     * signin authentication.
+     */
+    #[Test]
+    public function test_valid_settings_for_choice_based_signin(): void
+    {
+        $this->assertTrue($this->validateUserPoolClientConfig(
+            Enums\CognitoAuthFlowTypes::USER_AUTH));
+
+        // Set configuration to allow password authentication
+        Config::set('cognito.allow_passkeys', true);
+    } //Function ends
+
     #[Test]
     public function test_public_passkey_challenge_route_validates_required_payload(): void
     {
         $this->assertValidationError('POST', '/login/passkey/challenge');
-    }
+    } //Function ends
 
     #[Test]
     public function test_protected_passkey_routes_require_authentication(): void
@@ -29,5 +71,6 @@ class WebAuthPasskeyControllerApiTest extends ApiTestCase
         $this->assertUnauthenticated('GET', '/user/passkey/start');
         $this->assertUnauthenticated('POST', '/user/passkey/complete');
         $this->assertUnauthenticated('DELETE', '/user/passkey');
-    }
-}
+    } //Function ends
+
+} //Class ends
