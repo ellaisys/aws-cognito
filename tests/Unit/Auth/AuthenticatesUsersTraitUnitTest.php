c0ef75491fe597fa764d4c87463c653e623b531d
EllaiSys <admin@ellaisys.com>
2026-09-16 23:59:50 +0530

    fix: add test cases to increase the test coverage of the AuthenticatesUsers Trait

--- tests/Unit/Auth/AuthenticatesUsersTraitUnitTest.php
+++ tests/Unit/Auth/AuthenticatesUsersTraitUnitTest.php
@@ -28,9 +28,10 @@ use Ellaisys\Cognito\Tests\Support\BaseAuthTraitFixture;
 
 use Exception;
 use Illuminate\Validation\ValidationException;
+use Ellaisys\Cognito\Exceptions\AwsCognitoException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-#[Group('web'), Group('unit'), Group('auth-trait')]
+#[Group('web'), Group('unit'), Group('auth-trait'), Group('login'), Group('srp')]
 #[CoversTrait(AuthenticatesUsers::class)]
 class AuthenticatesUsersTraitUnitTest extends TestCase
 {
@@ -98,6 +99,24 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
     } // Function ends
 
     /**
+     * Test that the attemptLoginSRP method throws an AwsCognitoException for
+     * invalid config.
+     */
+    #[Test]
+    #[Depends('test_method_attemptLoginSRP_exists_in_class')]
+    public function test_method_attemptLoginSRP_with_invalid_config(): void
+    {
+        $this->expectException(AwsCognitoException::class);
+
+        $request = request()->merge([
+            'username' => 'valid_user',
+            'password' => 'valid_password',
+        ]);
+
+        $this->class->attemptLoginSRP($request);
+    } // Function ends
+
+    /**
      * Test that the challenge method exists in the class.
      */
     #[Test]
@@ -116,7 +135,7 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
     #[Test]
     #[DataProvider('challengeExceptionDataProvider')]
     #[Depends('test_method_challenge_exists_in_class')]
-    public function test_challenge_method_with_validation_exception(array $data,
+    public function test_challenge_method_with_exception(array $data,
         string $expectedException): void
     {
         $this->expectException($expectedException);
@@ -124,7 +143,7 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
     } // Function ends
 
     /**
-     * Data provider for test_challenge_method_with_validation_exception.
+     * Data provider for test_challenge_method_with_exception.
      */
     public static function challengeExceptionDataProvider(): array
     {
@@ -144,13 +163,13 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
             ],
             'invalid_challenge_data' => [
                 ['challenge_name' => 'INVALID_CHALLENGE',
-                'challenge_value' => 'dummy_value',
+                'challenge_value' => 'dummy_value1',
                 'session' => 'dummy_session'],
                 ValidationException::class
             ],
             'partial_challenge_data' => [
                 ['challenge_name' => 'SELECT_CHALLENGE',
-                'challenge_value' => 'dummy_value'],
+                'challenge_value' => 'dummy_value2'],
                 ValidationException::class
             ],
             'select_challenge_data' => [
@@ -161,19 +180,19 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
             ],
             'password_srp_wrong_format_data' => [
                 ['challenge_name' => 'PASSWORD_SRP',
-                'challenge_value' => 'dummy_value',
+                'challenge_value' => 'dummy_value3',
                 'session' => false],
                 ValidationException::class
             ],
             'password_srp_wrong_session_data' => [
                 ['challenge_name' => 'PASSWORD_SRP',
-                'challenge_value' => 'dummy_value',
+                'challenge_value' => 'dummy_value4',
                 'session' => ['dummy_session']],
                 \TypeError::class
             ],
             'password_verifier_wrong_format_data' => [
                 ['challenge_name' => 'PASSWORD_VERIFIER',
-                'challenge_value' => 'dummy_value',
+                'challenge_value' => 'dummy_value5',
                 'session' => 'dummy_session'],
                 ValidationException::class
             ],
@@ -183,9 +202,20 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
                 'session' => 'dummy_session'],
                 ValidationException::class
             ],
+            'password_verifier_invalid_data_correct_format' => [
+                ['challenge_name' => 'PASSWORD_VERIFIER',
+                'challenge_value' => '{"PASSWORD_CLAIM_SECRET_BLOCK":"1234",
+                "PRIVATE_KEY":"ed2241912d78c81a868f5f9d6607839f82292a9d5e6bcc01",
+                "PASSKEY_HASH":"1234", "TIMESTAMP":"1234"}',
+                'challenge_params' => '{"SALT":"abcd", "SRP_B":"1234",
+                "USER_ID_FOR_SRP":"test-user-id"}',
+                'session' => 'ed2241912d78c81a868f5f9d6607839f82292a9d5e6bcc01',
+                'username' => 'dummy_username'],
+                AwsCognitoException::class
+            ],
             'device_verifier_wrong_object_data' => [
                 ['challenge_name' => 'DEVICE_PASSWORD_VERIFIER',
-                'challenge_value' => '{"dummy_value":"abcd"}',
+                'challenge_value' => '{"some_dummy_value":"dcab"}',
                 'session' => 'dummy_session'],
                 ValidationException::class
             ]
@@ -203,6 +233,17 @@ class AuthenticatesUsersTraitUnitTest extends TestCase
             method_exists($this->class, 'logout'),
             'Method logout does not exist in the class.'
         );
+    } // Function ends
+
+    /**
+     * Test that the logout method throws an exception.
+     */
+    #[Test]
+    #[Depends('test_method_logout_exists_in_class')]
+    public function test_method_logout_throws_exception(): void
+    {
+        $this->expectException(Exception::class);
+        $this->class->logout(request());
     } // Function ends
 
 } // Class ends
